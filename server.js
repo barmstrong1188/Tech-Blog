2cbb637f8bf82e86acc1a5e6887764e7b9319690
Brandon Armstrong <65680645+barmstrong1188@users.noreply.github.com>
2020-08-29 11:33:28 -0400

    set up the server/database

---
+++ server.js
@@ -0,0 +1,11 @@
+//dependencies
+const express = require('express');
+const sequelize = require('./config/connection');
+
+const app = express();
+const PORT = process.env.PORT || 3001;
+
+//start the server
+app.listen(PORT, () => {
+    console.log('Server listening on : http://localhost:' + PORT);
+});
