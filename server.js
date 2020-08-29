ec580d979836db0ceeb57d394a9b13a539273b74
Brandon Armstrong <65680645+barmstrong1188@users.noreply.github.com>
2020-08-29 11:57:39 -0400

    made user model, need to add routes and test

--- server.js
+++ server.js
@@ -1,7 +1,7 @@
 //dependencies
 const express = require('express');
 const sequelize = require('./config/connection');
-
+const path = require('path');
 const app = express();
 const PORT = process.env.PORT || 3001;
 
