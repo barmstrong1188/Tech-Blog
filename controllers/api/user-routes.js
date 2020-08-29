d7600effaa67fe8b00c41000d162fa908221eee7
Brandon Armstrong <65680645+barmstrong1188@users.noreply.github.com>
2020-08-29 14:13:57 -0400

    set up api/index, pseudocode for user routes

---
+++ controllers/api/user-routes.js
@@ -0,0 +1,10 @@
+const router = require('express').Router();
+const { User } = require('../../models');
+
+//get route to /api/users  --get all users
+//get users by id
+//post /api/users ---create user
+//put /api/users --update user
+//delete /api/users  --delete user
+
+//export
