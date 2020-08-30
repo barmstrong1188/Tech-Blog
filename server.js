ea46686f950379ac8aae4d66f133b4fe940810d7
Brandon Armstrong <65680645+barmstrong1188@users.noreply.github.com>
2020-08-30 18:48:09 -0400

    started on homepage template, loads in browser. needs to be styled.

--- server.js
+++ server.js
@@ -1,15 +1,23 @@
 //dependencies
 const express = require('express');
-const routes = require('./controllers')
+const routes = require('./controllers/')
 const sequelize = require('./config/connection');
-// const path = require('path');
+const path = require('path');
 const app = express();
+const exphbs = require('express-handlebars');
+const hbs = exphbs.create({});
 const PORT = process.env.PORT || 3001;
+
+app.engine('handlebars', hbs.engine);
+app.set('view engine', 'handlebars');
 
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
+app.use(express.static(path.join(__dirname, 'public')));
 
 app.use(routes);
+
+
 
 //start the server
 sequelize.sync({ force: true }).then(() => {
