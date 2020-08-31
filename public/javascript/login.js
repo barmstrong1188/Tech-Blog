b5125353ccffa70131cf527f6f34632a810f3c78
Brandon Armstrong <65680645+barmstrong1188@users.noreply.github.com>
2020-08-31 00:22:48 -0400

    added dashboard template/routes

--- public/javascript/login.js
+++ public/javascript/login.js
@@ -17,33 +17,8 @@ const loginFormHandler = async function(event) {
       .catch(err => console.log(err));
   };
   
+  document
+    .querySelector("#login-form")
+    .addEventListener("submit", loginFormHandler);
   
-  async function signupFormHandler(event) {
-    event.preventDefault();
-  
-    const username = document.querySelector('#username-signup').value.trim();
-    
-    const password = document.querySelector('#password-signup').value.trim();
-  
-    if (username && password) {
-      const response = await fetch('/api/users', {
-        method: 'post',
-        body: JSON.stringify({
-          username,
-          password
-        }),
-        headers: { 'Content-Type': 'application/json' }
-      });
-  
-      if (response.ok) {
-        document.location.replace('/dashboard/');
-      } else {
-        alert(response.statusText);
-      }
-    }
-  }
-  
-  document.querySelector('.login-form').addEventListener('submit', loginFormHandler);
-  
-  document.querySelector('.signup-form').addEventListener('submit', signupFormHandler);
   
